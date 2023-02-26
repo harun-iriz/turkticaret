7733cbbf283596d5b7030431f5e11a6855c03220
harun-iriz <haruniriz@gmail.com>
2023-02-26 17:33:09 +0300

    Added Cache for Products
    Cache for Products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use App\Events\ProductCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $dispatchesEvents = [
+        'created' => ProductCreated::class
+    ];
 }
